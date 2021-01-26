f24b1425b633db2f0156dd4ebef623d2eb4e998c
John Trujillo <amriikk@gmail.com>
2021-01-25 16:57:21 -0800

    seed friend data

---
+++ data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'Petey Pablo',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+      id: 2,
+      name: 'Nicki Putaj',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Putrixie Little',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Putiana Walsh',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Lola Culetto',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
