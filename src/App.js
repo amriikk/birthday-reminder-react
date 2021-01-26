d35cddb52ba57f8344cbc722899f78deb7b864b4
John Trujillo <amriikk@gmail.com>
2021-01-25 17:45:50 -0800

    update className for section

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [people, setPeople] = useState(data);
   return (
     <main>
-      <section>
+      <section className='container'>
         <h3>{people.length} Birthdays Today</h3>
         <List people={people} />
         <button onClick={ () => setPeople([])}>Clear All</button>
